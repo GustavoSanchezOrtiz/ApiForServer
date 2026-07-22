aef0d7daa9119f2b27bdb1f0c7e2fa807334be6e
Gustavo Sanchez O <gustavo_3141240147@utd.edu.mx>
2026-07-22 16:28:33 -0600

    limit rate y agragado de cors

--- main.py
+++ main.py
@@ -2,15 +2,38 @@ from fastapi import FastAPI
 from psutil import virtual_memory, disk_usage, cpu_percent, net_io_counters, boot_time
 import datetime 
 inicio = datetime.datetime.fromtimestamp(boot_time())
-
-
+from fastapi.middleware.cors import CORSMiddleware
+from slowapi import Limiter
+from slowapi.util import get_remote_address
+from slowapi.errors import RateLimitExceeded
+from slowapi.middleware import SlowAPIMiddleware
+from fastapi import Request
 app = FastAPI()
+limiter = Limiter(key_func=get_remote_address)
+
+app.state.limiter = limiter
+app.add_exception_handler(RateLimitExceeded, limiter._rate_limit_exceeded_handler)
+app.add_middleware(SlowAPIMiddleware)
+
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "https://server.kunibo.net",
+    ],
+    allow_credentials=False,
+    allow_methods=["GET"],
+    allow_headers=["*"],
+)
 
 @app.get("/status")
-def status():
+@limiter.limit("35/minute")
+def status(request: Request):
     memory = virtual_memory()
     disk = disk_usage("/")
     cpu = cpu_percent()
+
     return {
         "status": "online",
         "memory": {
